8f835f246b14b13e3c3ce848a263d8ed753cefd5
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2026-06-28 15:27:42 +0800

    revert carry tweak to rebalance for stealth

--- lua/carrytweakdata.lua
+++ lua/carrytweakdata.lua
@@ -1,6 +1,6 @@
-Hooks:PostHook(CarryTweakData, "init", "init_sss", function (self)
-	for _, v in pairs(self.types) do
-		v.can_run = true
-		v.jump_modifier = v.move_speed_modifier ^ 0.5
-	end
-end)
+-- Hooks:PostHook(CarryTweakData, "init", "init_sss", function (self)
+-- 	for _, v in pairs(self.types) do
+-- 		v.can_run = true
+-- 		v.jump_modifier = v.move_speed_modifier ^ 0.5
+-- 	end
+-- end)
